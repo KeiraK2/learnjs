ba1ec3de36c329cf946a9fce023c03d70f5e81d9
keira.kajari@tptlive.ee <keira.kajari@tptlive.ee>
2025-10-12 15:35:42 +0300

    add types example

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("Hello, World!"); 
+var num = 1; // don't use var anymore
+let num2 = 2; 
+console.log(num2, num);
+num = 3.4;
+console.log(typeof num);
+num = 3;
+console.log(typeof num);
+let text = "Keira";
+console.log(typeof text);   
+let bool = true;
+console.log(typeof bool);
+bool = false;
+console.log(typeof bool);
+let nothing;
+console.log(typeof nothing);   
+nothing = undefined;
+nothing = null;
+console.log(typeof nothing);  
+let array = [1, 2, 3, 4, 'sad', true, 3.5]; 
+console.log(typeof array);
+let obj = {name: "Keira", 
+    age: 17, 
+    isStudent: true,
+data:{
+    phone: '123-456-7890',
+    email: 'keira.kajari@tptlive.ee'
+
+}
+};
+console.log(typeof obj);
+
+function hello (){
+    console.log("Hello from function");
+}
+
+console.log(typeof hello);
