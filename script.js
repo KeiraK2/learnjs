2a9cc0784bba34847829cd2c3a9d46e299723501
keira.kajari@tptlive.ee <keira.kajari@tptlive.ee>
2025-10-14 09:53:26 +0300

    Add array examples

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ let array = [1, 2, 3, 4];
 console.log(array);
 console.log(array[0]);
 array[2] = 10;
+console.log(array);
+array = ['hello', 1, 2, 3, true, [4,5,6]];
+console.log(array);
+console.log(array[5][1]);
+array = 
+    [
+    
+        [1,2,3], 
+        [1,2,3], 
+        [1,2,3]
+    ],
+    [
+        [1,2,3], 
+        [1,2,3], 
+        [1,2,3]
+    ],
+    [
+        [1,2,3], 
+        [1,2,3], 
+        [1,2,3]
+];
+array = [[[[[1]]]]];
+console.log(array[0][0][0][0][0]);
+array = [1, 2, 3, 4];
+console.log(array.length);
+console.log(array.join('0'));
