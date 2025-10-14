2f0664c0b69fb0411750ebfb22d76cf802d1cee3
keira.kajari@tptlive.ee <keira.kajari@tptlive.ee>
2025-10-14 09:42:45 +0300

    Add loop examples

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-for(let i=0; i<10; i++) {
-    console.log(i);
-}
-
-for(let i=9; i<10; i++) {
-    console.log(i);
-}
-
-for(let i=1; i< 1_000_000; i*=2) {  
-    console.log(i);
-}
-
-for (let i=0; i>10; i++) {
-    console.log(i);
-}   
-
-//for(let i=0; i<10; i--) {
-//    console.log(i);
-// }
-
-let start = Date.now();
-console.log(start);
-let i = 0;
-while(Date.now() < start + 5000) {
-    i++;
-}
-console.log(i);
-
-// while(true) {
-
-// }
-
-let num = 5;
-
-while(num>5) {
-    console.log('WHILE');
-}
-
-do {
-    console.log('DO');
-} while(num>5);
+let array = [1, 2, 3, 4];
+console.log(array);
+console.log(array[0]);
+array[2] = 10;
