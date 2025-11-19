4dba93c2d7b0f297a071eb1acf3d2c29e47d42ab
keira.kajari@tptlive.ee <keira.kajari@tptlive.ee>
2025-11-19 19:22:54 +0200

    Add commonjs vs esmodule

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-console.log(window, document);
-let heading = document.querySelector('h1');
-console.log({heading});
-heading.innerText = 'Hello DOM!';
+console.log('Hello terminal!');
 
-let button = document.querySelector('button');
+//const common = require('./common.js');
+const common = require('./common.js');
 
-function getRandomColor() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+common.hello();
 
-button.addEventListener('click', event => {
-    console.log(event);
-    let color = getRandomColor();
-    heading.innerText = 'Such click, Much wow';
-    heading.style.color = getRandomColor();
-});
+import es from './module.js';
 
-setInterval(() => {
-    document.documentElement.style.backgroundColor = getRandomColor();
-}, 10000);
-
-let input = document.querySelector('input');
-
-input.addEventListener('input', event => {
-    console.log(event);
-    heading.innerText = input.value.split('').reverse().join('');
-});
+es.hello();
