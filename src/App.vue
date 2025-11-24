75be20895893b4776ee8e9f4e967a11aae1e7896
keira.kajari@tptlive.ee <keira.kajari@tptlive.ee>
2025-11-24 13:31:06 +0200

    Add vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+    let message = ref('Hello Vue');
+    let color = ref(false)
+</script>
+
+<template>
+   <div class="consent">
+      <button class="button is-primary" @click="message = 'Hello VM24'">Click Me!</button>
+      <h1>{{ message }}</h1>
+      <button class="button" :class="{'is-danger' : isDanger, 'is-warning': !isDanger}" @click="isDanger = !isDanger">Cool button</button>
+    </div>
+</template>
