628fbc4511ac8c7218654ef7b5bba9c08065171b
keira.kajari@tptlive.ee <keira.kajari@tptlive.ee>
2025-12-15 16:50:42 +0200

    Add Modal example

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,20 @@
 <script setup>
-import { computed, ref } from 'vue';
-import ItemList from './ItemList.vue';
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
 
-let message = ref('Hello Vue');
-let isDanger = ref(false);
-let newItem = ref('Viin');
-let i = 0;
-let items = ref([
-    {id: i++, name: 'Õlu', isDone: true},
-    {id: i++, name: 'Monsu', isDone: false},
-    {id: i++, name: 'Sai', isDone: true},
-    {id: i++, name: 'Leib',isDone: false},
-]);
 
-function add() {
-    if(newItem.value.trim() !== ''){
-       items.value.push({id: i++, name: newItem.value, isDone: false});
-    }
-    newItem.value = '';
-}
-
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-let toDoItems = computed(() => items.value.filter(item => !items.isDone));
-
+let modal1Active = ref(false);
+let modal2Active = ref(false);
 </script>
 
 <template>
     <div class="container mt-2">
         <div class="content">
-            <button class="button is-primary" @click="message = 'Hello VM24'">Click Me!</button>
-            <h1>{{ message }}</h1>
-            <button class="button" :class="{ 'is-danger' : isDanger, 'is-warning': !isDanger }" 
-            @click="isDanger = !isDanger">Cool button</button>
+        <button class="button is-primary" @click="modal1Active = true">Open Modal 1</button>
+        <button class="button is-link" @click="modal2Active = true">Open Modal 2</button>
 
-
-            <div class="field has-addons mt-2">
-                <div class="control is-expanded">
-                    <input v-model="newItem" @keydown.enter="add" class="input" type="text" placeholder="Add Item">
-                </div>
-                <div class="control">
-                    <button class="button is-info" @click="add">
-                        Add Item
-                    </button>
-                </div>
-            </div>
-            <h1>{{ newItem.split('').reverse().join('') }}</h1>
-
-
-            <ItemList :items="items" title="All Items"></ItemList>
-            <ItemList :items="doneitems" title="Done Items"></ItemList>
-            <ItemList :items="toDoItems" title="ToDo Items"></ItemList>
+            <Modal :active="modal1Active" @close="modal1Active = false" url="https://picsum.photos/1280/960?random=1"></Modal>
+            <Modal :active="modal2Active" @close="modal2Active = false" url="https://picsum.photos/1280/960?random=2"></Modal>
         </div>
     </div>
 </template>
---
+++ src/pages/todo.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { computed, ref } from 'vue';
+import ItemList from './components/ItemList.vue';
+
+let message = ref('Hello Vue');
+let isDanger = ref(false);
+let newItem = ref('Viin');
+let i = 0;
+let items = ref([
+    {id: i++, name: 'Õlu', isDone: true},
+    {id: i++, name: 'Monsu', isDone: false},
+    {id: i++, name: 'Sai', isDone: true},
+    {id: i++, name: 'Leib',isDone: false},
+]);
+
+function add() {
+    if(newItem.value.trim() !== ''){
+       items.value.push({id: i++, name: newItem.value, isDone: false});
+    }
+    newItem.value = '';
+}
+
+let doneItems = computed(() => items.value.filter(item => item.isDone));
+let toDoItems = computed(() => items.value.filter(item => !items.isDone));
+
+</script>
+
+<template>
+    <div class="container mt-2">
+        <div class="content">
+            <button class="button is-primary" @click="message = 'Hello VM24'">Click Me!</button>
+            <h1>{{ message }}</h1>
+            <button class="button" :class="{ 'is-danger' : isDanger, 'is-warning': !isDanger }" 
+            @click="isDanger = !isDanger">Cool button</button>
+
+
+            <div class="field has-addons mt-2">
+                <div class="control is-expanded">
+                    <input v-model="newItem" @keydown.enter="add" class="input" type="text" placeholder="Add Item">
+                </div>
+                <div class="control">
+                    <button class="button is-info" @click="add">
+                        Add Item
+                    </button>
+                </div>
+            </div>
+            <h1>{{ newItem.split('').reverse().join('') }}</h1>
+
+            <ItemList :items="items" title="All Items"></ItemList>
+            <ItemList :items="doneitems" title="Done Items"></ItemList>
+            <ItemList :items="toDoItems" title="ToDo"></ItemList>
+        </div>
+    </div>
+</template>
